e5a1774326a6f26314f2d783d1376d271fee3c92
Ablaye-Fall <ablayefallaye@gmail.com>
2025-08-11 14:08:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -153,6 +153,8 @@ def qa_multilang(user_question):
     ranked = rerank_results(question_albanian, passages)
     best_passages = ranked[:3]  # ✅ Correction : pas de p[0]
     combined = " ".join(best_passages)
+    # Nettoyage
+    cleaned = nettoyer_html(best_passage)
 
     # Traduction vers langue originale
     if lang_detected != "sq":
